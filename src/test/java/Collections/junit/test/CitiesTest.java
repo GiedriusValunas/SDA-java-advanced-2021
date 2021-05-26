85702a1332654912e2b5f50172b4e33336b64335
wsaulius <veinsreider@google.com>
2021-05-26 18:55:43 +0300

    Adding tests

---
+++ src/test/java/Collections/junit/test/CitiesTest.java
@@ -0,0 +1,32 @@
+package Collections.junit.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.Collections;
+
+class CitiesTest {
+
+    @Test
+    void verifyExample1() {
+    }
+
+    @Test
+    void verifyExample2() {
+    }
+
+    @Test
+    void verifyExample3() {
+    }
+
+    @Test
+    void whenExample1() {
+    }
+
+    @Test
+    void whenExample2() {
+    }
+
+}
